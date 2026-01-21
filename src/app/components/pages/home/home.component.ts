f71586cd199b43695026600eb7642ba0ff8e1dd5
LebohangMonamane <lebohang@bsc.africa>
2026-01-21 15:08:14 +0200

    Done

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [CommonModule]
 })
-export class HomeComponent { }
+export class HomeComponent {
+  imageLoading = {
+    profile: true,
+    agric: true,
+    intern: true,
+    fansdor: true
+  };
+
+  onImageLoad(imageKey: string) {
+    this.imageLoading[imageKey as keyof typeof this.imageLoading] = false;
+  }
+
+  onImageError(event: any) {
+    event.target.src = '/logo.png';
+  }
+}
